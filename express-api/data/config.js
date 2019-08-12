5ed64c0214c6501818ded515ca574c6165d1fd70
Kelly Yen <t-keyen@microsoft.com>
2019-08-12 12:58:21 -0700

    added tab to nav bar

---
+++ express-api/data/config.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql'); 
+
+const config = {
+    host: 'localhost', 
+    user: 'root', 
+    password: 'root', 
+    database: 'api', 
+}
+
+const pool = mysql.createPool(config); 
+module.exports = pool; 
